fbca109311937724f5c8be6e31e4c2c8f4ca5989
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-30 04:32:04 +0600

    Food items loaded

--- src/Components/FoodItem/FoodItem.jsx
+++ src/Components/FoodItem/FoodItem.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Card, Col } from 'react-bootstrap';
-import './FoodItem.css'
+import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
+import "./FoodItem.css";
+import { FcGlobe } from "react-icons/fc";
+import { FaTags } from "react-icons/fa";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import { SiIfood } from "react-icons/si";
 
 const FoodItem = ({ food }) => {
-    const {/* strArea, idMeal, */ strMeal, strMealThumb} = food
-    return (
-        
+  const { strArea, strMeal, strMealThumb, strTags, strYoutube } = food;
+  return (
     <Col>
       <Card>
         <Card.Img variant="top" src={strMealThumb} />
         <Card.Body>
           <Card.Title>{strMeal}</Card.Title>
           <Card.Text>
-           
+            <p>
+              <FcGlobe /> - {strArea}
+            </p>
+            <p>
+              <FaTags /> - {strTags ? strTags : "Not Available"}
+            </p>
           </Card.Text>
         </Card.Body>
+              <div className="flex">
+              <a href={strYoutube} className="w-50 btn btn-primary">
+              <BsFillPlayCircleFill />
+            </a>
+            <Button variant="success" className="w-50">
+              <SiIfood />
+            </Button>
+           </div>
       </Card>
     </Col>
-
-    );
+  );
 };
 
 export default FoodItem;
